68d2bb307a02100be8d6aa4f7473741b7fa28fa5
Eunice Olowookere <eunice.olowookere@ucdconnect.ie>
2023-02-21 23:54:18 +0000

    Test commit

--- Software Engineering Project.py
+++ Software Engineering Project.py
@@ -2,4 +2,4 @@ import requests
 link = 'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=5ac1b590c7e338d66c34fa0c5610e94c99a3b45c'
 getStaticData = requests.get(link)
 staticDataFile = getStaticData.json()
-staticDataFile
+staticDataFile # test commit by Eunice
