1a74d49bea600b4f0f84f12d503cf5c14359e1a1
Eunice Olowookere <eunice.olowookere@ucdconnect.ie>
2023-03-05 19:59:10 +0000

    flask project begin

--- weatherApi.py
+++ weatherApi.py
@@ -7,8 +7,9 @@ root_url = "http://maps.openweathermap.org/maps/2.0/weather"
 # City name for which we need the weather data
 city_name = "dublin"
 # Building the final url for the API call
-url = f"{root_url}appid={api_key}&q={city_name}"
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&appid={api_key}"
 # sending a get request at the url
 r = requests.get(url)
 # displaying the json weather data returned by the api
 print(r.json())
+
