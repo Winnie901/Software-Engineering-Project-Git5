b82a999485657b2ac7e23778133e6c1e95fcbe34
iVandalise <36800115+iVandalise@users.noreply.github.com>
2023-04-03 23:15:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 db_params = {
-    'host': 'localhost',
+    'host': 'localhost', # I think this needs to be changed to the full filepath of the aws location
     'port': 3306,
     'user': 'SEGroup25',
     'password': 'SEGroup25?',
@@ -21,7 +21,7 @@ db_url = f"mysql://{db_params['SEGroup25']}:{db_params['SEGroup25?']}@{db_params
 # Create a MySQL connection pool
 pool = pymysql.create_pool(host=db_params['localhost'], port=db_params['3306'], user=db_params['SEGroup25'], password=db_params['SEGroup25?'], db=db_params['localhost'], charset=db_params['utf8mb4'])
 
-@app.route('/')
+@app.route('/map') # This has been changed as we are specifying the 'map.html' file in our folder
 def index():
     # Get a connection from the pool
     conn = pool.connection()
@@ -29,9 +29,48 @@ def index():
 
     # Execute a SELECT query
     cursor.execute("SELECT * FROM mytable")
+    
+    # My version of executing the SELECT query
+    # cursor.execute('SELECT * FROM availability ORDER BY last_update DESC LIMIT 115')
+    # This should return the last 115 entries into the database, which should be the most
+    # up to date availability numbers for the bikes
+
+    
 
     # Fetch the results and store them in a list
     rows = cursor.fetchall()
+    
+    
+    # I then grabbed the location values for all the rows in the station table, in order to plot
+    # the markers
+    
+    # Getting the locations
+    # cursor.execute('SELECT * FROM station')
+    # location_results = cursor.fetchall()
+
+    # Now we will be extracting all the lat and long values
+    # locations = []
+    # for location in location_results:
+        # latitude = location[7]
+        # longitude = location[8]
+        # name = location[5]
+        
+        # bikes_available = None
+        # for availability in availability_results:
+            # if availability[0] == location[6]:
+                # bikes_available = availability[1]
+                # break
+        
+
+        
+        # locations.append((latitude, longitude, bikes_available, name))
+
+    # return render_template('map.html', API_KEY='AIzaSyCmEmTVXz4FLSsTM3JME9J3VW-WXECqmKw', locations=locations)
+    
+    
+    
+    
+    
 
     # Close the connection
     conn.close()
