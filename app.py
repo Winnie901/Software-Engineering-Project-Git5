77fe9c7b6143b60b63a1e597ff7c6250fb0701b6
iVandalise <36800115+iVandalise@users.noreply.github.com>
2023-04-21 14:50:56 +0100

    Comment update
    Commented all the parts I know, just need to comment the 2 routes:
    - /standsavailability/<int:stand_id>
    - /availability/<int:station_id>

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import jsonify
 import sklearn
 from flask_sqlalchemy import model
 
+# Loading in the training models that we will be using later
 bike_model = pickle.load(open('bikes_model.pkl', 'rb'))
 stands_model = pickle.load(open('stands_model.pkl','rb'))
 
@@ -31,43 +32,53 @@ conn = pymysql.connect(
 
 )
 
+# Creating the route for the main page the user will access
 @app.route('/')
 def index():
+    # cursor that will execute the SQL query to the database
     cursor = conn.cursor()
+    
+    # Selecting the last 117 entries from the availability table
     cursor.execute('SELECT * FROM availability ORDER BY last_update DESC LIMIT 117')
-
-
     availability_results = cursor.fetchall()
 
-
+    # Selecting all the information from the stations table
     cursor.execute('SELECT * FROM stations')
     location_results = cursor.fetchall()
+    
+    # Debugging code print statements to make sure the query executed successfully
     print(availability_results)
     print(location_results)
 
+    
+    # Gathering station coordinate, name, and number values
     locations = []
     for location in location_results:
         latitude = location['position_lat']
         longitude = location['position_long']
         name = location['stat_name']
         number = location['number']
-
+        
+        # Gathering available bike values and available bike stand values
         bikes_available = None
         for availability in availability_results:
             if availability['number'] == location['number']:
                 bikes_available = availability['available_bikes']
                 bike_stands_available = availability['available_bike_stands']
                 break
-
+        
+        # Adding each to the list locations
         locations.append((latitude, longitude, bikes_available, name, bike_stands_available,number))
-
+    
+    # Returning all information which will be used in conjunction with adding markers to a map and displaying window information
     return render_template('index.html', API_KEY='AIzaSyCmEmTVXz4FLSsTM3JME9J3VW-WXECqmKw', locations=locations)
 
 
-
+# Route for when the user wants to route from one station to another
 @app.route('/mapping.html')
 def map():
-
+    
+    # Exact same code as the the previous route
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM availability ORDER BY last_update DESC LIMIT 117')
 
@@ -98,12 +109,13 @@ def map():
     return render_template('mapping.html', API_KEY='AIzaSyCmEmTVXz4FLSsTM3JME9J3VW-WXECqmKw', locations=locations)
 
 
+# Route that will return the news portion of the site
 @app.route('/news.html')
 def news():
     return render_template('news.html')
 
 
-
+# Route that will return the how-to portion of the site
 @app.route('/how-to.html')
 def howto():
     return render_template('how-to.html')
@@ -140,6 +152,6 @@ def predict_stands(stand_id):
 
     return json.dumps(json_dict)
 
-
+# Start the application
 if __name__ == "__main__":
     app.run(host ="0.0.0.0", port =8080, debug = True)
